5a85bd75cb0e5ba7921dde351f9daa5ed7ced882
ThakurParam <thakurranaji2001@gmail.com>
2025-01-04 17:42:36 +0530

    push after scroll effect

--- assets/Layout.js
+++ assets/Layout.js
@@ -5,26 +5,26 @@ import { Container } from "@mui/material";
 import React, { useEffect } from "react";
 
 export default function Layout({ children }) {
-  useEffect(() => {
-    let scrollTimeout;
+  // useEffect(() => {
+  //   let scrollTimeout;
 
-    const smoothScroll = (event) => {
-      clearTimeout(scrollTimeout);
-      scrollTimeout = setTimeout(() => {
-        window.scrollBy({
-          top: event.deltaY,
-          behavior: "smooth",
-        });
-      }, 0);
-    };
+  //   const smoothScroll = (event) => {
+  //     clearTimeout(scrollTimeout);
+  //     scrollTimeout = setTimeout(() => {
+  //       window.scrollBy({
+  //         top: event.deltaY,
+  //         behavior: "smooth",
+  //       });
+  //     }, 0);
+  //   };
 
-    window.addEventListener("wheel", smoothScroll);
+  //   window.addEventListener("wheel", smoothScroll);
 
-    return () => {
-      window.removeEventListener("wheel", smoothScroll);
-      clearTimeout(scrollTimeout);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("wheel", smoothScroll);
+  //     clearTimeout(scrollTimeout);
+  //   };
+  // }, []);
 
   return (
     <>
